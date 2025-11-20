9ddf22607171b3319b9a034bd522bd4e091ef6f9
Kocsis Gergely <kocsisger@example.com>
2025-11-20 11:41:40 +0100

    Person ok

--- src/main/java/hu/unideb/inf/bootstrap_cs10/model/Person.java
+++ src/main/java/hu/unideb/inf/bootstrap_cs10/model/Person.java
@@ -3,12 +3,14 @@ package hu.unideb.inf.bootstrap_cs10.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "persons")
 @Data
 @Builder
 @NoArgsConstructor
