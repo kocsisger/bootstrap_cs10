b2a35e2c72e52072a5d7a5c4d3c86144f2e3be18
Kocsis Gergely <kocsisger@example.com>
2025-11-20 11:16:47 +0100

    Init commit

---
+++ src/main/java/hu/unideb/inf/bootstrap_cs10/BootstrapCs10Application.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_cs10;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootstrapCs10Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootstrapCs10Application.class, args);
+    }
+
+}
---
+++ src/test/java/hu/unideb/inf/bootstrap_cs10/BootstrapCs10ApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_cs10;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootstrapCs10ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
