aeb30642a9a0595b0987b6b7548fa245fa73a831
Kocsis Gergely <kocsisger@example.com>
2025-11-27 10:27:45 +0100

    css

---
+++ src/main/java/hu/unideb/inf/bootstrap_cs10/controller/MainController.java
@@ -0,0 +1,13 @@
+package hu.unideb.inf.bootstrap_cs10.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String showHomepage () {
+        return "index";
+    }
+
+}
